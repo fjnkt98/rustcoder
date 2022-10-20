a3d6d2f205cf8734c9eef26a6c6490035637ff98
fjnkt98 <fjnkt98@gmail.com>
2022-10-20 23:25:41 +0900

    update

--- math-and-algorithm/src/bin/034.rs
+++ math-and-algorithm/src/bin/034.rs
@@ -1,7 +1,22 @@
+use itertools::Itertools;
 use proconio::input;
+use std::cmp;
 
 fn main() {
     input! {
-        a: i32,
+        n: usize,
+        xy: [(i64, i64); n]
     };
+
+    let mut answer = 1 << 60;
+    for cmb in (0..n).combinations(2) {
+        let (x1, y1) = xy[cmb[0]];
+        let (x2, y2) = xy[cmb[1]];
+
+        let dist = (x1 - x2).pow(2) + (y1 - y2).pow(2);
+
+        answer = cmp::min(answer, dist);
+    }
+
+    println!("{}", (answer as f64).sqrt());
 }
