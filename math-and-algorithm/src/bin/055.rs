b06cb7308ae79cc9938ae07d23365fd3f2bf4197
fjnkt98 <fjnkt98@gmail.com>
2022-10-24 23:17:58 +0900

    add

--- math-and-algorithm/src/bin/055.rs
+++ math-and-algorithm/src/bin/055.rs
@@ -1,7 +1,42 @@
+extern crate nalgebra as na;
 use proconio::input;
+
+fn matrix_multiply(a: &na::Matrix2<i64>, b: &na::Matrix2<i64>) -> na::Matrix2<i64> {
+    let p = 1000_000_007;
+
+    let c = a * b;
+    return c.map(|x| x % p);
+}
+
+fn matrix_power(m: &na::Matrix2<i64>, n: i64) -> na::Matrix2<i64> {
+    let mut p: na::Matrix2<i64> = m.clone();
+    let mut q: na::Matrix2<i64> = na::Matrix2::new(0, 0, 0, 0);
+
+    let mut flag = false;
+
+    for i in 0..60 {
+        if (n & (1 << i)) != 0 {
+            if flag {
+                q = matrix_multiply(&q, &p);
+            } else {
+                q = p.clone();
+                flag = true;
+            }
+        }
+
+        p = matrix_multiply(&p, &p);
+    }
+
+    return q;
+}
 
 fn main() {
     input! {
-        a: i32,
+        n: i64,
     };
+
+    let a: na::Matrix2<i64> = na::Matrix2::new(2, 1, 1, 0);
+    let b = matrix_power(&a, n - 1);
+    let answer = (b[(1, 0)] + b[(1, 1)]) % 1000000007;
+    println!("{}", answer);
 }
--- math-and-algorithm/src/bin/056.rs
+++ math-and-algorithm/src/bin/056.rs
@@ -1,7 +1,42 @@
+extern crate nalgebra as na;
 use proconio::input;
+
+fn matrix_multiply(a: &na::Matrix3<i64>, b: &na::Matrix3<i64>) -> na::Matrix3<i64> {
+    let p = 1000_000_007;
+
+    let c = a * b;
+    return c.map(|x| x % p);
+}
+
+fn matrix_power(m: &na::Matrix3<i64>, n: i64) -> na::Matrix3<i64> {
+    let mut p: na::Matrix3<i64> = m.clone();
+    let mut q: na::Matrix3<i64> = na::Matrix3::zeros();
+
+    let mut flag = false;
+
+    for i in 0..60 {
+        if (n & (1 << i)) != 0 {
+            if flag {
+                q = matrix_multiply(&q, &p);
+            } else {
+                q = p.clone();
+                flag = true;
+            }
+        }
+
+        p = matrix_multiply(&p, &p);
+    }
+
+    return q;
+}
 
 fn main() {
     input! {
-        a: i32,
+        n: i64,
     };
+
+    let a: na::Matrix3<i64> = na::Matrix3::new(1, 1, 1, 1, 0, 0, 0, 1, 0);
+    let b = matrix_power(&a, n - 1);
+    let answer = b.row(2).dot(&na::Vector3::from([2, 1, 1]).transpose()) % 1000000007;
+    println!("{}", answer);
 }
