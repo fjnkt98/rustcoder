b239912460ac852bd355a2c582a44e31c982c60b
fjnkt98 <fjnkt98@gmail.com>
2022-11-12 16:43:42 +0900

    add

--- tessoku-book/src/bin/a46.rs
+++ tessoku-book/src/bin/a46.rs
@@ -1,7 +1,49 @@
 use proconio::input;
 
+fn distance(a: (f64, f64), b: (f64, f64)) -> f64 {
+    return ((a.0 - b.0).powf(2.0) + (a.1 - b.1).powf(2.0)).sqrt();
+}
+
 fn main() {
     input! {
-        n: i64,
+        n: usize,
+        xy: [(f64, f64); n]
     };
+
+    let mut answer: Vec<usize> = Vec::new();
+    answer.push(0);
+    let mut explored = vec![false; n];
+    explored[0] = true;
+    let mut current = xy[0];
+    for _ in 0..n {
+        let mut next: usize = n;
+        let mut d = std::f64::INFINITY;
+        for i in 0..n {
+            if explored[i] {
+                continue;
+            }
+            if distance(current, xy[i]) < d {
+                d = distance(current, xy[i]);
+                next = i;
+            }
+        }
+
+        if next == n {
+            break;
+        }
+
+        answer.push(next);
+        current = xy[next];
+        explored[next] = true;
+    }
+    answer.push(0);
+
+    println!(
+        "{}",
+        answer
+            .iter()
+            .map(|x| (x + 1).to_string())
+            .collect::<Vec<String>>()
+            .join("\n")
+    );
 }
