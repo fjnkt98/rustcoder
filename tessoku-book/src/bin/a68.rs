446daace73e8ad576971b79a6be7e85e0027a601
fjnkt98 <fjnkt98@gmail.com>
2022-11-20 12:33:36 +0900

    refactor

--- tessoku-book/src/bin/a68.rs
+++ tessoku-book/src/bin/a68.rs
@@ -1,49 +1,78 @@
 use proconio::input;
 
 #[derive(Clone, Debug, Copy)]
+#[allow(dead_code)]
 struct Edge {
-    pub capacity: i64,
+    pub from: usize,
     pub to: usize,
+    pub capacity: i64,
     pub rev: usize,
 }
 
 #[derive(Debug, Clone)]
+#[allow(dead_code)]
 struct Graph {
-    v: Vec<Vec<Edge>>,
-    flow: i64,
+    pub n: usize,
+    pub m: usize,
+    pub edges: Vec<Vec<Edge>>,
 }
 
 impl Graph {
-    fn new(n: usize) -> Graph {
-        let v = vec![Vec::new(); n];
-        return Graph { v, flow: 0 };
+    pub fn new(n: usize, edges: &Vec<(usize, usize, i64)>, zero_indexed: bool) -> Graph {
+        let mut graph = vec![Vec::new(); n];
+        match zero_indexed {
+            true => {
+                for (from, to, capacity) in edges.iter() {
+                    let rev_position_from = graph[*to].len();
+                    let rev_position_to = graph[*from].len();
+
+                    graph[*from].push(Edge {
+                        from: *from,
+                        to: *to,
+                        capacity: *capacity,
+                        rev: rev_position_from,
+                    });
+
+                    graph[*to].push(Edge {
+                        from: *to,
+                        to: *from,
+                        capacity: 0,
+                        rev: rev_position_to,
+                    });
+                }
+            }
+            false => {
+                for (from, to, capacity) in edges.iter() {
+                    let from = *from - 1;
+                    let to = *to - 1;
+                    let rev_position_from = graph[to].len();
+                    let rev_position_to = graph[from].len();
+
+                    graph[from].push(Edge {
+                        from: from,
+                        to: to,
+                        capacity: *capacity,
+                        rev: rev_position_from,
+                    });
+
+                    graph[to].push(Edge {
+                        from: to,
+                        to: from,
+                        capacity: 0,
+                        rev: rev_position_to,
+                    });
+                }
+            }
+        };
+
+        return Graph {
+            n: n,
+            m: edges.len(),
+            edges: graph,
+        };
     }
 
-    fn add(&mut self, a: usize, b: usize, c: i64) {
-        let rev_position_a = self.v[b].len();
-        let rev_position_b = self.v[a].len();
-
-        self.v[a].push(Edge {
-            capacity: c,
-            to: b,
-            rev: rev_position_a,
-        });
-
-        self.v[b].push(Edge {
-            capacity: 0,
-            to: a,
-            rev: rev_position_b,
-        });
-    }
-
-    fn find_path(&mut self, start: usize, end: usize) -> i64 {
-        let mut explored = vec![false; self.v.len()];
-        explored[start] = true;
-
-        return self.find_path_recursive(&mut explored, start, end, 1i64 << 60);
-    }
-
-    fn find_path_recursive(
+    fn dfs(
         &mut self,
         explored: &mut Vec<bool>,
         current: usize,
@@ -54,8 +83,8 @@ impl Graph {
             return minimum_flow;
         }
 
-        for i in 0..self.v[current].len() {
-            let edge = self.v[current][i];
+        for i in 0..self.edges[current].len() {
+            let edge = self.edges[current][i];
 
             if explored[edge.to] || edge.capacity == 0 {
                 continue;
@@ -63,7 +92,7 @@ impl Graph {
 
             explored[edge.to] = true;
 
-            let flow = self.find_path_recursive(
+            let flow = self.dfs(
                 explored,
                 edge.to,
                 destination,
@@ -71,8 +100,8 @@ impl Graph {
             );
 
             if flow > 0 {
-                self.v[current][i].capacity -= flow;
-                self.v[edge.to][edge.rev].capacity += flow;
+                self.edges[current][i].capacity -= flow;
+                self.edges[edge.to][edge.rev].capacity += flow;
                 return flow;
             }
         }
@@ -80,10 +109,20 @@ impl Graph {
         return 0;
     }
 
-    fn add_flow(&mut self, start: usize, destination: usize) -> i64 {
-        let flow = self.find_path(start, destination);
-        self.flow += flow;
-        return flow;
+    fn get_minimum_flow(&mut self, start: usize, destination: usize) -> i64 {
+        let mut minimum_flow: i64 = 0;
+        loop {
+            let mut explored = vec![false; self.n];
+
+            let flow = self.dfs(&mut explored, start, destination, 1i64 << 60);
+            if flow > 0 {
+                minimum_flow += flow;
+            } else {
+                break;
+            }
+        }
+
+        return minimum_flow;
     }
 }
 
@@ -94,20 +133,7 @@ fn main() {
         abc: [(usize, usize, i64); m]
     };
 
-    let mut graph = Graph::new(n);
+    let mut graph = Graph::new(n, &abc, false);
 
-    for &(a, b, c) in abc.iter() {
-        let a = a - 1;
-        let b = b - 1;
-        graph.add(a, b, c);
-    }
-
-    loop {
-        let flow = graph.add_flow(0, n - 1);
-        if flow == 0 {
-            break;
-        }
-    }
-
-    println!("{}", graph.flow);
+    println!("{}", graph.get_minimum_flow(0, n - 1));
 }
